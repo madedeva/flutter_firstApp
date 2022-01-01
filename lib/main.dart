1dab25d3c48fc0fb3c198888360ccf730d8ea43f
madedeva <deva.kerti@undiksha.ac.id>
2022-01-01 16:47:10 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome"),
+        ),
+        body: Center(
+            child: Container(
+              color: Colors.lightBlue,
+              width: 150,
+              height: 50,
+              child: Text("Get Started"))),
+      ),
+    );
+  }
+}
+
+
